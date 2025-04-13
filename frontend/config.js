a523b0a8c1d9ede8c428012424f15b829a46fc1b
Marshal Alone <marshal3869@gmail.com>
2025-04-13 10:47:14 +0530

    b

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 const config = {
     API_URL: window.location.hostname === 'localhost'
         ? 'http://localhost:3000'
-        : 'https://collaboard-backend-cdr6.onrender.com'
+        : 'https://whiteboard-backend-p0l5.onrender.com/'
 };
 
 export default config; 
